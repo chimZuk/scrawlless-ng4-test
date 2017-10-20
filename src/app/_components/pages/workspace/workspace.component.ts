4ee45e3c9bbbf2968cf27de00ade8f67f8c88fd3
kuzya19989 <kuzya19989@outlook.com>
2017-10-20 05:03:02 -0400

    Cross browser SVG

--- src/app/_components/pages/workspace/workspace.component.ts
+++ src/app/_components/pages/workspace/workspace.component.ts
@@ -17,6 +17,15 @@ export class WorkspaceComponent implements OnInit {
   sub: any;
   pageName: any;
 
+  getArr = function (i) {
+    return new Array(i);
+  }
+
+  lineHeight(i){
+    console.log(Number(i))
+    return Number(i) * Number(100)
+  }
+
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = +params['id'];
