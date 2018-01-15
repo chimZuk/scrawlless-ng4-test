89056b21ef67d82427613bbacff0b18d6d01e136
kuzya19989 <kuzya19989@outlook.com>
2018-01-15 01:07:43 -0500

    Fixed bug with touch drag

--- src/app/_components/pages/workspace/workspace.component.ts
+++ src/app/_components/pages/workspace/workspace.component.ts
@@ -33,16 +33,12 @@ export class WorkspaceComponent implements OnInit {
   selected: any = [];
 
   isFullscreen = false;
-
-  //@ViewChild('cover') cover: ElementRef;
   @ViewChild('container') container: ElementRef;
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     //this.vw = window.innerWidth;
     //this.vh = window.innerHeight;
-    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
-    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
   }
 
   getArr = function (i) {
@@ -68,7 +64,6 @@ export class WorkspaceComponent implements OnInit {
         }
       }
     }
-    console.log(this.selected);
   }
 
   notSaved = function (array, element) {
@@ -87,8 +82,6 @@ export class WorkspaceComponent implements OnInit {
   zoom(scale) {
     this.container.nativeElement.attributes.width.value *= scale;
     this.container.nativeElement.attributes.height.value *= scale;
-    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
-    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
   }
 
 
@@ -99,34 +92,7 @@ export class WorkspaceComponent implements OnInit {
 
     this.vw = window.innerWidth;
     this.vh = window.innerHeight;
-    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
-    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
 
-    /*interact('.draggable')
-      .draggable({
-        inertia: true,
-        restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-        autoScroll: true,
-        onmove: dragMoveListener,
-        onend: function (event) {
-        }
-      });
-
-    function dragMoveListener(event) {
-      var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-      target.style.webkitTransform =
-        target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    }*/
     var container = this.container.nativeElement;
     var selected = this.selected;
     console.log(container);
@@ -139,48 +105,25 @@ export class WorkspaceComponent implements OnInit {
         onmove: dragMoveListener,
         onend: function (event) {
         }
-      }).on("tap", function(){
+      }).on("tap", function () {
         console.log("tap")
       });
 
     function dragMoveListener(event) {
+      console.log(event);
       var target = event.target,
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-      //target.style.webkitTransform =
-      //target.style.transform =
-      //'translate(' + x + 'px, ' + y + 'px)';
-      //target
-      //;
+
+      var cW = Number(container.attributes.width.value);
+      var cH = Number(container.attributes.height.value);
+      var cA = container.attributes.viewBox.value.split(' ');
 
       for (var i = 0; i < selected.length; i++) {
-        selected[i].attributes.cx.value = String(Number(selected[i].attributes.cx.value) + Number(x / 2));
-        selected[i].attributes.cy.value = String(Number(selected[i].attributes.cy.value) + Number(y / 2))
+        selected[i].attributes.cx.value = String(Number(selected[i].attributes.cx.value) + Number(x) * (cA[2] / cW));
+        selected[i].attributes.cy.value = String(Number(selected[i].attributes.cy.value) + Number(y) * (cA[2] / cH))
       }
-
-      // target.setAttribute('cx', x);
-      // target.setAttribute('cy', y);
     }
-
-
-    /*interact("#container")
-      .gesturable({
-        onstart: function (event) {
-          event.target.classList.add('touch');
-          cover.classList.add('touch');
-        },
-        onmove: function (event) {
-          scale = scale * (1 + event.ds);
-
-          cover.style.webkitTransform =
-            cover.style.transform =
-            'scale(' + scale + ')';
-        },
-        onend: function (event) {
-          console.log(cover);
-
-        }
-      });*/
   }
 
   SX: any;
