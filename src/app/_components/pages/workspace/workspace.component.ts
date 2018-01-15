fb71cfa3b902770a0ad93f7513338b8cbe71fc79
kuzya19989 <kuzya19989@outlook.com>
2018-01-15 00:42:12 -0500

    workspace version 1.0.0

--- src/app/_components/pages/workspace/workspace.component.ts
+++ src/app/_components/pages/workspace/workspace.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild, HostListener, AfterViewInit } from '@angular/core';
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Location } from '@angular/common';
 
 import * as interact from 'interactjs';
 
@@ -12,8 +14,11 @@ import * as interact from 'interactjs';
 export class WorkspaceComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private location: Location
   ) { }
+
+  absUrl = window.location.href;
 
   private id;
   sub: any;
@@ -25,49 +30,79 @@ export class WorkspaceComponent implements OnInit {
   sheetDrag: any;
   sheetDragEnabled = true;
 
+  selected: any = [];
+
+  isFullscreen = false;
+
+  //@ViewChild('cover') cover: ElementRef;
+  @ViewChild('container') container: ElementRef;
+
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.vw = window.innerWidth;
-    this.vh = window.innerHeight;
-    this.cover.nativeElement.style.width = String(this.vh * 0.8) + 'px';
-    this.cover.nativeElement.style.height = String(this.vh) + 'px';
+    //this.vw = window.innerWidth;
+    //this.vh = window.innerHeight;
+    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
+    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
   }
 
   getArr = function (i) {
     return new Array(i);
   }
 
-  dragSwitch = function () {
-    if (this.sheetDragEnabled) {
-      this.sheetDrag[0].disable();
-      this.sheetDragEnabled = false;
+  select = function (event) {
+    if (this.notSaved(this.selected, event.target)) {
+
+      event.target.attributes.class.value = "svgDraggable touch";
+      event.target.setAttribute('stroke-width', "3");
+      event.target.attributes.r.value = String(Number(event.target.attributes.r.value) + 1);
+      this.selected.push(event.target);
+      this.container.nativeElement.attributes.class.value = "touch";
     } else {
-      this.sheetDrag[0].enable();
-      this.sheetDragEnabled = true;
+      if (!this.notSaved(this.selected, event.target)) {
+        event.target.attributes.class.value = "";
+        event.target.setAttribute('stroke-width', "1");
+        event.target.attributes.r.value = String(Number(event.target.attributes.r.value) - 1);
+        this.selected.splice(this.selected.indexOf(event.target), 1);
+        if (this.selected.length == 0) {
+          this.container.nativeElement.attributes.class.value = "";
+        }
+      }
     }
+    console.log(this.selected);
   }
 
-  /*zoom = function (x) {
-    TweenLite.to("#container", 0.2, { zoom: x });
-    this.value = x;
-  }*/
+  notSaved = function (array, element) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  @ViewChild('cover') cover: ElementRef;
+  touched() {
+
+  }
+
+  zoom(scale) {
+    this.container.nativeElement.attributes.width.value *= scale;
+    this.container.nativeElement.attributes.height.value *= scale;
+    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
+    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
+  }
+
+
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = +params['id'];
     });
 
-    var scale = 1;
-    var cover = this.cover.nativeElement;
     this.vw = window.innerWidth;
     this.vh = window.innerHeight;
+    //this.cover.nativeElement.style.left = String((this.vw / 2) - (this.cover.nativeElement.clientWidth / 2)) + 'px';
+    //this.cover.nativeElement.style.top = String((this.vh / 2) - (this.cover.nativeElement.clientHeight / 2)) + 'px';
 
-    cover.style.width = String(this.vh * 0.8) + 'px';
-    cover.style.height = String(this.vh) + 'px';
-
-    // target elements with the "draggable" class
-    interact('.draggable')
+    /*interact('.draggable')
       .draggable({
         inertia: true,
         restrict: {
@@ -78,28 +113,57 @@ export class WorkspaceComponent implements OnInit {
         autoScroll: true,
         onmove: dragMoveListener,
         onend: function (event) {
-          console.log("Drag ended");
         }
       });
 
     function dragMoveListener(event) {
       var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      // translate the element
       target.style.webkitTransform =
         target.style.transform =
         'translate(' + x + 'px, ' + y + 'px)';
-
-      // update the posiion attributes
       target.setAttribute('data-x', x);
       target.setAttribute('data-y', y);
+    }*/
+    var container = this.container.nativeElement;
+    var selected = this.selected;
+    console.log(container);
+    interact('.svgDraggable')
+      .draggable({
+        autoScroll: true,
+        onstart: function (event) {
+
+        },
+        onmove: dragMoveListener,
+        onend: function (event) {
+        }
+      }).on("tap", function(){
+        console.log("tap")
+      });
+
+    function dragMoveListener(event) {
+      var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      //target.style.webkitTransform =
+      //target.style.transform =
+      //'translate(' + x + 'px, ' + y + 'px)';
+      //target
+      //;
+
+      for (var i = 0; i < selected.length; i++) {
+        selected[i].attributes.cx.value = String(Number(selected[i].attributes.cx.value) + Number(x / 2));
+        selected[i].attributes.cy.value = String(Number(selected[i].attributes.cy.value) + Number(y / 2))
+      }
+
+      // target.setAttribute('cx', x);
+      // target.setAttribute('cy', y);
     }
 
 
-    interact("#container")
+    /*interact("#container")
       .gesturable({
         onstart: function (event) {
           event.target.classList.add('touch');
@@ -111,140 +175,12 @@ export class WorkspaceComponent implements OnInit {
           cover.style.webkitTransform =
             cover.style.transform =
             'scale(' + scale + ')';
-
-          //dragMoveListener(event);
         },
         onend: function (event) {
           console.log(cover);
 
         }
-      });
-
-    /*interact('.svg-draggable')
-      .draggable({
-        inertia: true,
-        autoScroll: true,
-        onmove: svgDragMoveListener,
-        onend: function (event) {
-          console.log("Drag ended");
-        }
-      })
-      .gesturable({
-        onstart: function (event) {
-
-        },
-        onmove: function (event) {
-          var isScale = false;
-          var target = event.target;
-          var currentMatrix;
-          var transMatrix = target.getAttributeNS(null, "transform").slice(7, -1).split(' ');
-          if (transMatrix[0] <= 4 && transMatrix[0] >= 0.5) {
-            isScale = true;
-          } else {
-            isScale = false;
-          }
-          scale = scale * (1 + event.ds);
-          if (isScale || !isScale && (transMatrix[0] > 4 && scale < 1 || transMatrix[0] < 0.5 && scale > 1)) {
-            if (scale > 1.005) {
-              scale = 1.005;
-            } else {
-              if (scale < 0.995) {
-                scale = 0.995;
-              }
-            }
-            for (var i = 0; i < transMatrix.length; i++) {
-              transMatrix[i] *= scale;
-            }            
-            target.setAttributeNS(null, "transform", "matrix(" + transMatrix.join(' ') + ")");
-            currentMatrix = target.getAttributeNS(null, "transform").slice(7, -1).split(' ');
-            for (var i = 0; i < currentMatrix.length; i++) {
-              currentMatrix[i] = parseFloat(currentMatrix[i]);
-            }
-            var width = target.getAttributeNS(null, "width");
-            var height = target.getAttributeNS(null, "height");
-            currentMatrix[4] += (1 - scale) * width / 6;
-            currentMatrix[5] += (1 - scale) * height / 4;
-            var newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-            target.setAttributeNS(null, "transform", newMatrix);
-          }
-          svgDragMoveListener(event);
-        },
-        onend: function (event) {
-
-        }
-      });
-
-    function svgDragMoveListener(event) {
-      var target = event.target;
-      var currentMatrix;
-      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-      currentMatrix = target.getAttributeNS(null, "transform").slice(7, -1).split(' ');
-      for (var i = 0; i < currentMatrix.length; i++) {
-        currentMatrix[i] = parseFloat(currentMatrix[i]);
-      }
-
-      currentMatrix[4] += x;
-      currentMatrix[5] += y;
-      var newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-      target.setAttributeNS(null, "transform", newMatrix);
-    }*/
-    /*Draggable.create(".box1", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: ".container1",
-      throwProps: true
-    });
-
-    Draggable.create(".box2", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: ".container2",
-      throwProps: true
-    });
-
-    Draggable.create(".box3", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: ".container3",
-      throwProps: true
-    });
-    Draggable.create(".box4", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: ".container4",
-      throwProps: true
-    });*/
-
-    /*Draggable.create("#dragSwitch", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: "workspace",
-      throwProps: true
-    });
-
-    Draggable.create(".zoomButtons", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: "workspace",
-      throwProps: true
-    });*/
-
-    /*Draggable.create("#cover", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: "#main",
-      throwProps: true
-    });*/
-
-    /*this.sheetDrag = Draggable.create("#cover", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: "#container",
-      throwProps: true
-    });*/
-
+      });*/
   }
 
   SX: any;
