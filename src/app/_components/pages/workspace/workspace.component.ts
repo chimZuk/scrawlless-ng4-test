118a2c2e28ccabde0d6668be2c3b5cf9946ac877
kuzya19989 <kuzya19989@outlook.com>
2018-02-18 22:51:35 -0500

    Fractions module update stage

--- src/app/_components/pages/workspace/workspace.component.ts
+++ src/app/_components/pages/workspace/workspace.component.ts
@@ -76,6 +76,7 @@ export class WorkspaceComponent implements OnInit {
     0: {
       line: 0,
       x: 0,
+      pos: 0,
       pe: 0,
       s: 0,
       value: "",
@@ -87,6 +88,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 1,
       s: 2,
+      pos: 1,
       value: "-1",
       text: "&#xe90b;&#xe900;",
       type: "digit"
@@ -96,6 +98,7 @@ export class WorkspaceComponent implements OnInit {
       x: 2,
       pe: 1,
       s: 1,
+      pos: 2,
       value: "+",
       text: "&#xe90a;",
       type: "operator"
@@ -105,15 +108,18 @@ export class WorkspaceComponent implements OnInit {
       x: 3,
       pe: 1,
       s: 8,
+      pos: 3,
       value: "",
       text: "",
-      type: "fraction"
+      type: "fraction",
+      fr: 1
     },
     4: {
       line: 0,
       x: 11,
       pe: 1,
       s: 1,
+      pos: 4,
       value: "+",
       text: "&#xe90a;",
       type: "operator"
@@ -123,6 +129,7 @@ export class WorkspaceComponent implements OnInit {
       x: 12,
       pe: 1,
       s: 1,
+      pos: 5,
       value: "8",
       text: "&#xe907;",
       type: "digit"
@@ -132,15 +139,18 @@ export class WorkspaceComponent implements OnInit {
       x: 13,
       pe: 1,
       s: 2,
+      pos: 6,
       value: "",
       text: "",
-      type: "fraction"
+      type: "fraction",
+      fr: 4
     },
     7: {
       line: 0,
       x: 0,
       pe: 2,
       s: 1,
+      pos: 1,
       value: "2",
       text: "&#xe901;",
       type: "digit"
@@ -150,6 +160,7 @@ export class WorkspaceComponent implements OnInit {
       x: 1,
       pe: 2,
       s: 1,
+      pos: 2,
       value: "-",
       text: "&#xe90b;",
       type: "operator"
@@ -159,15 +170,18 @@ export class WorkspaceComponent implements OnInit {
       x: 2,
       pe: 2,
       s: 5,
+      pos: 3,
       value: "",
       text: "",
-      type: "fraction"
+      type: "fraction",
+      fr: 2
     },
     10: {
       line: 0,
       x: 0,
       pe: 4,
       s: 1,
+      pos: 1,
       value: "4",
       text: "&#xe903;",
       type: "digit"
@@ -177,6 +191,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 5,
       s: 1,
+      pos: 1,
       value: "5",
       text: "&#xe904;",
       type: "digit"
@@ -186,6 +201,7 @@ export class WorkspaceComponent implements OnInit {
       x: 1,
       pe: 5,
       s: 1,
+      pos: 2,
       value: "*",
       text: "&#xe90c;",
       type: "operator"
@@ -195,15 +211,18 @@ export class WorkspaceComponent implements OnInit {
       x: 2,
       pe: 5,
       s: 2,
+      pos: 3,
       value: "",
       text: "",
-      type: "fraction"
+      type: "fraction",
+      fr: 3
     },
     14: {
       line: 0,
       x: 0,
       pe: 6,
       s: 1,
+      pos: 1,
       value: "6",
       text: "&#xe905;",
       type: "digit"
@@ -213,6 +232,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 7,
       s: 1,
+      pos: 1,
       value: "7",
       text: "&#xe906;",
       type: "digit"
@@ -222,6 +242,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 3,
       s: 1,
+      pos: 1,
       value: "3",
       text: "&#xe902;",
       type: "digit"
@@ -231,6 +252,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 8,
       s: 1,
+      pos: 1,
       value: "8",
       text: "&#xe907;",
       type: "digit"
@@ -240,6 +262,7 @@ export class WorkspaceComponent implements OnInit {
       x: 0,
       pe: 9,
       s: 1,
+      pos: 1,
       value: "9",
       text: "&#xe908;",
       type: "digit"
@@ -263,7 +286,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 0,
       pd: 0,
       fr: 0,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 0,
       ce: [1],
       cd: []
@@ -278,7 +301,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 0,
       pd: 0,
       fr: 0,
-      ch: 0, zn: 1,
+      ch: 0, zn: 0, osn: 1,
       s: 16,
       cs: 15,
       ce: [2, 3, 8, 9],
@@ -294,7 +317,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 1,
       pd: 3,
       fr: 1,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 8,
       cs: 7,
       ce: [4, 5],
@@ -310,7 +333,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 1,
       pd: 3,
       fr: 1,
-      ch: 0, zn: 1,
+      ch: 0, zn: 1, osn: 0,
       s: 8,
       cs: 1,
       ce: [],
@@ -326,7 +349,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 2,
       pd: 9,
       fr: 2,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 5,
       cs: 1,
       ce: [],
@@ -342,7 +365,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 2,
       pd: 9,
       fr: 2,
-      ch: 0, zn: 1,
+      ch: 0, zn: 1, osn: 0,
       s: 5,
       cs: 4,
       ce: [6, 7],
@@ -358,7 +381,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 5,
       pd: 13,
       fr: 3,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 2,
       cs: 1,
       ce: [],
@@ -374,7 +397,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 5,
       pd: 13,
       fr: 3,
-      ch: 0, zn: 1,
+      ch: 0, zn: 1, osn: 0,
       s: 2,
       cs: 1,
       ce: [],
@@ -390,7 +413,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 1,
       pd: 6,
       fr: 4,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 2,
       cs: 1,
       ce: [],
@@ -406,7 +429,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 1,
       pd: 6,
       fr: 4,
-      ch: 0, zn: 1,
+      ch: 0, zn: 1, osn: 0,
       s: 2,
       cs: 1,
       ce: [],
@@ -425,7 +448,7 @@ export class WorkspaceComponent implements OnInit {
       pe: 0,
       pd: 0,
       ch: 0,
-      zn: 0,
+      zn: 1,
       isActive: 0
     },
     1: {
@@ -495,26 +518,73 @@ export class WorkspaceComponent implements OnInit {
     }
   }
 
+  setChildExpressionS(ch) {
+    for (let i = 0; i < ch.length; i++) {
+      if (ch != 0) {
+        this.ex[ch[i]].s++;
+      }
+    }
+  }
+
+  setChildExpressionX(ch, POS) {
+    for (let i = 0; i < ch.length; i++) {
+      if (this.di[ch[i]].pos > POS) {
+        if (POS > 0) {
+          this.di[ch[i]].x++;
+        }
+        if (this.di[ch[i]].type == "fraction") {
+          if (POS > 0) {
+            this.ex[this.fr[this.di[ch[i]].fr].ch].x++;
+            this.ex[this.fr[this.di[ch[i]].fr].zn].x++;
+          }
+          this.setChildExpressionX(this.ex[this.fr[this.di[ch[i]].fr].ch].cd, 0);
+          this.setChildExpressionX(this.ex[this.fr[this.di[ch[i]].fr].zn].cd, 0);
+        }
+      }
+    }
+  }
+
   writeFr() {
     var exp = this.selection.ex;
     var line = this.selection.line;
     //Add fraction related digit
     //---->>>>
     //#region
+    var POS = this.ex[exp].cd.length + 1;
     var di = {
       line: line,
       x: this.ex[exp].cs,
       pe: exp,
       s: 1,
+      pos: POS,
       value: "",
       text: "",
-      type: "fraction"
+      type: "fraction",
+      fr: this.elements.fractions.length + 1
     }
     var newDi = this.elements.digits.length + 1;
+    this.ex[exp].cd.push(newDi);
     this.elements.digits.push(newDi);
     this.lines[line].digits.push(newDi);
     this.di[newDi] = di;
     //#endregion
+    //----<<<<
+
+    //Modify parent expression height
+    //---->>>>
+    //#region
+    var bottomExpression = false;
+    if (this.ex[exp].zn == 1 && this.ex[this.ex[exp].pe].b - this.ex[exp].h > 0) {
+      bottomExpression = true;
+    }
+    var heightChanged = false;
+    if (this.ex[exp].t == 0) {
+      this.ex[exp].h += 2;
+      this.ex[exp].t++;
+      this.ex[exp].b++;
+      heightChanged = true;
+    }
+    //endregion
     //----<<<<
 
     //Add new fraction expressions
@@ -530,7 +600,7 @@ export class WorkspaceComponent implements OnInit {
       pe: exp,
       pd: newDi,
       fr: 0,
-      ch: 1, zn: 0,
+      ch: 1, zn: 0, osn: 0,
       s: 1,
       cs: 0,
       ce: [],
@@ -546,7 +616,7 @@ export class WorkspaceComponent implements OnInit {
       pe: exp,
       pd: newDi,
       fr: 0,
-      ch: 0, zn: 1,
+      ch: 0, zn: 1, osn: 0,
       s: 1,
       cs: 0,
       ce: [],
@@ -556,11 +626,17 @@ export class WorkspaceComponent implements OnInit {
     var topExpression = false;
     switch (this.ex[exp].ch) {
       case 1: {
-        if (this.ex[exp].y != this.ex[this.ex[exp].pe].y) {
+        var CID = exp;
+        while (CID != 0 && this.ex[CID].zn != 1) {
+          if (this.ex[exp].y > this.ex[CID].y) {
+            topExpression = true;
+          }
+          CID = this.ex[CID].pe;
+        }
+        if (topExpression && heightChanged) {
           ch.y = this.ex[exp].y - 2;
           zn.y = this.ex[exp].y;
           this.ex[exp].y -= 2;
-          topExpression = true;
         } else {
           ch.y = this.ex[exp].y;
           zn.y = this.ex[exp].y + 2;
@@ -613,34 +689,24 @@ export class WorkspaceComponent implements OnInit {
     //#endregion
     //----<<<<
 
-    //Modify parent expression
+    //Modify parent expression length and content length
     //---->>>>
     //#region
-    var heightChanged = false;
-    if (this.ex[exp].t == 0) {
-      this.ex[exp].h += 2;
-      this.ex[exp].t++;
-      this.ex[exp].b++;
-      heightChanged = true;
-    }
-
+    var lengthChanged = false;
     this.ex[exp].cs++;
-    if (this.ex[exp].cs == this.di[this.ex[exp].pd].s) {
-      this.di[this.ex[exp].pd].s++;
-    }
     if (this.ex[exp].cs == this.ex[exp].s) {
       this.ex[exp].s++;
+      lengthChanged = true;
     }
     //endregion
     //----<<<<
 
-    //Modify all parent expressions
+    //Modify all parent expressions height
     //---->>>>
     //#region
     var CID = exp;
-    if (heightChanged && !topExpression) {
+    if (heightChanged && !topExpression && !bottomExpression) {
       while (CID != 0) {
-
         switch (this.ex[CID].ch) {
           case 1: {
             this.ex[this.ex[CID].pe].t += 2;
@@ -657,7 +723,6 @@ export class WorkspaceComponent implements OnInit {
           default:
             break;
         }
-
         var childExpressions = [];
         this.ex[this.ex[CID].pe].ce.forEach(el => {
           childExpressions.push(el);
@@ -667,15 +732,79 @@ export class WorkspaceComponent implements OnInit {
         if (this.ex[CID].zn == 1) {
           childExpressions = [];
         }
-
         this.setChildExpressionY(childExpressions);
-        
+        CID = this.ex[CID].pe;
+      }
+    } else {
+      if (topExpression) {
+        while (CID != 0) {
+          switch (this.ex[CID].ch) {
+            case 1: {
+              if (this.ex[this.ex[CID].pe].t - (this.ex[CID].h - 1) < 0) {
+                this.ex[this.ex[CID].pe].y -= 2;
+                this.ex[this.ex[CID].pe].t += 2;
+                this.ex[this.ex[CID].pe].h += 2;
+              }
+
+              break;
+            }
+            case 0: {
+              if (this.ex[this.ex[CID].pe].b <= this.ex[CID].h - 3) {
+                this.ex[this.ex[CID].pe].b += 2;
+                this.ex[this.ex[CID].pe].h += 2;
+              }
+              break;
+            }
+            default:
+              break;
+          }
+          CID = this.ex[CID].pe;
+        }
+      }
+    }
+    //endregion
+    //----<<<<
+
+    //Modify all parent expressions length
+    //---->>>>
+    //#region
+    var CID = exp;
+    if (lengthChanged) {
+      while (CID != 0) {
+        if (CID != exp) {
+          this.ex[CID].cs++;
+        }
+
+
+        var childExpressions = [];
+        childExpressions.push(this.fr[this.ex[CID].fr].ch);
+        childExpressions.push(this.fr[this.ex[CID].fr].zn);
+        var currentExpressionIndex = childExpressions.indexOf(exp);
+        if (currentExpressionIndex != -1) {
+          childExpressions.splice(currentExpressionIndex, 1);
+        }
+        this.setChildExpressionS(childExpressions);
+
+
+        var childDigits = [];
+        this.ex[CID].cd.forEach(cd => {
+          childDigits.push(cd);
+        });
+        if (childDigits.length > 0) {
+          this.setChildExpressionX(childDigits, POS);
+        }
+
+        if (this.ex[CID].cs == this.di[this.ex[CID].pd].s) {
+          this.di[this.ex[CID].pd].s++;
+        }
+
+
+        POS = this.di[this.ex[CID].pd].pos;
         CID = this.ex[CID].pe;
       }
     }
     //endregion
     //----<<<<
-    console.log(this.ex);
   }
 
   writeEx(id) {
